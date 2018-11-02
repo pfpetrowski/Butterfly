e79968abb7f7ecb2b87687b517796f0610ab8d28
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-11-01 20:59:57 -0500

    Network Still Bad

--- buzz_classifier.py
+++ buzz_classifier.py
@@ -5,9 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 
 train_images = ImageDataGenerator(
 	rescale = 1./255,
-	horizontal_flip = True,
-	fill_mode = 'nearest',
-	width_shift_range = [-0.5,0])
+	horizontal_flip = True)
 
 val_images = ImageDataGenerator(rescale = 1./255)
 
@@ -17,7 +15,7 @@ traindata = train_images.flow_from_directory(
 	'./Spectrograms/Train',
 	color_mode = 'grayscale',
 	target_size = (200, 200),
-	batch_size = 30,
+	batch_size = 90,
 	class_mode = 'binary')
 
 
@@ -35,20 +33,21 @@ valdata = val_images.flow_from_directory(
 	batch_size = 20,
 	class_mode = 'binary')
 
+
 from keras import models
 from keras import layers
 
 
-
-
 model = models.Sequential()
-model.add(layers.Conv2D(12, (2,2), activation = 'relu', input_shape = (200,200,1)))
+model.add(layers.Conv2D(3, (2,2), activation = 'relu', input_shape = (200,200,1)))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(24, (2,2), activation = 'relu'))
+model.add(layers.Conv2D(6, (2,2), activation = 'relu'))
 model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(24, (2,2), activation = 'relu'))
+model.add(layers.Conv2D(6, (2,2), activation = 'relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(6, (2,2), activation = 'relu'))
 model.add(layers.Flatten())
-model.add(layers.Dense(24, activation = 'relu'))
+model.add(layers.Dense(6, activation = 'relu'))
 model.add(layers.Dense(1, activation = 'softmax'))
 
 
@@ -62,7 +61,7 @@ model.compile(
 	)
 model.fit_generator(
 	traindata,
-	steps_per_epoch = 3,
+	steps_per_epoch = 1,
 	epochs = 1000,
 	validation_data = valdata,
 	validation_steps = 1)
