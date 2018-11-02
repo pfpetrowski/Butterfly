026229860da27929d3d9043433c99b778931ab11
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-11-02 09:24:57 -0500

    Aesthetic Changes

--- buzz_classifier.py
+++ buzz_classifier.py
@@ -2,14 +2,11 @@ import keras
 from keras.preprocessing.image import ImageDataGenerator
 
 
-
 train_images = ImageDataGenerator(
 	rescale = 1./255,
 	horizontal_flip = True)
 
 val_images = ImageDataGenerator(rescale = 1./255)
-
-
 
 traindata = train_images.flow_from_directory(
 	'./Spectrograms/Train',
@@ -19,19 +16,20 @@ traindata = train_images.flow_from_directory(
 	class_mode = 'binary')
 
 
-# for data_batch, labels_batch in traindata:
-# 	print('data batch shape:', data_batch.shape)
-# 	print('labels batch shape:', labels_batch.shape)
-# 	print(data_batch[1,:,:,0].shape)
-# 	break
-
-
 valdata = val_images.flow_from_directory(
 	'./Spectrograms/Validate',
 	color_mode = 'grayscale',
 	target_size = (200, 200),
 	batch_size = 20,
 	class_mode = 'binary')
+
+
+# for data_batch, labels_batch in traindata:
+# 	print('data batch shape:', data_batch.shape)
+# 	print('labels batch shape:', labels_batch.shape)
+# 	print(data_batch[1,:,:,0].round(1))
+# 	break
+# quit()
 
 
 from keras import models
@@ -59,6 +57,8 @@ model.compile(
 	loss = 'binary_crossentropy',
 	metrics = ['accuracy']
 	)
+
+
 model.fit_generator(
 	traindata,
 	steps_per_epoch = 1,
