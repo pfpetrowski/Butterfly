8ed1b622797a288db0d7a9c89f0a0acf0cc9b7bd
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-11-03 02:01:39 -0500

    Crushed it

--- buzz_classifier.py
+++ buzz_classifier.py
@@ -7,13 +7,15 @@ train_images = ImageDataGenerator(
 	horizontal_flip = True
 	)
 
-val_images = ImageDataGenerator(rescale = 1./255)
+val_images = ImageDataGenerator(
+	rescale = 1./255
+	)
 
 traindata = train_images.flow_from_directory(
 	'./Spectrograms/Train',
 	color_mode = 'grayscale',
 	target_size = (100, 100),
-	batch_size = 32,
+	batch_size = 64,
 	class_mode = 'binary')
 
 
@@ -24,25 +26,25 @@ valdata = val_images.flow_from_directory(
 	batch_size = 32,
 	class_mode = 'binary')
 
-import numpy
-numpy.set_printoptions(threshold=numpy.nan)
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
+# import numpy
+# numpy.set_printoptions(threshold=numpy.nan)
+# import matplotlib.pyplot as plt
+# from keras.preprocessing import image
 
 # for i, j in valdata:
 # 	print(i[9,:,:,0].round(1))
 # 	break
 # quit()
 
-for data_batch, labels_batch in traindata:
-	print('data batch shape:', data_batch.shape)
-	print('labels batch shape:', labels_batch.shape)
-	var = data_batch[3,:,:,0].round(1)
-	plt.imshow(image.array_to_img(data_batch[4]))
-	break
+# for data_batch, labels_batch in traindata:
+# 	print('data batch shape:', data_batch.shape)
+# 	print('labels batch shape:', labels_batch.shape)
+# 	var = data_batch[3,:,:,0].round(1)
+# 	plt.imshow(image.array_to_img(data_batch[4]))
+# 	break
 
-plt.show()	
-quit()
+#plt.show()	
+#quit()
 
 
 from keras import models
@@ -51,13 +53,17 @@ from keras import layers
 
 model = models.Sequential()
 model.add(layers.Conv2D(3, (2,2), activation = 'relu', input_shape = (100,100,1), padding = 'same'))
+model.add(layers.BatchNormalization())
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(6, (2,2), activation = 'relu', padding = 'same'))
+model.add(layers.BatchNormalization())
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(12, (2,2), activation = 'relu', padding = 'same'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D(2,2))
 model.add(layers.Flatten())
 model.add(layers.Dense(32, activation = 'relu'))
-model.add(layers.Dense(1, activation = 'softmax'))
+model.add(layers.Dense(1, activation = 'sigmoid'))
 
 
 # model.add(layers.Flatten(input_shape = (100,100,1)))
