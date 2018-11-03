f8ca3640646aa5a00cecfc0b8b0572082e5cef56
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-11-02 21:40:34 -0500

    more debugging. Also image plotting

--- audioparser.sh
+++ audioparser.sh
@@ -1,15 +1,15 @@
 n=1
 for i in `cat ./nonbuzz_starts.txt | uniq`;
 	do 
-		echo sox Colony3Day170.wav -n remix 2 trim $i 0.1 spectrogram -x 200 -y 200 -m -r -o ./Spectrograms/NonBuzzes/nonbuzz$n.png
-		sox Colony3Day170.wav -n remix 2 trim $i 0.1 spectrogram -x 200 -y 200 -m -r -o ./Spectrograms/NonBuzzes/nonbuzz$n.png &
+		echo sox processing nonbuzz from Colony3Day170.wav at time point $n ($i)
+		sox Colony3Day170.wav -n remix 2 trim $i 0.1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/NonBuzzes/nonbuzz$n.png &
 		n=`expr $n + 1`
 	done
 
 n=1
 for i in `cat ./buzz_starts.txt | uniq`;
 	do 
-		echo sox Colony3Day170.wav -n remix 2 trim $i 0.1 spectrogram -x 200 -y 200 -m -r -o ./Spectrograms/Buzzes/buzz$n.png
-		sox Colony3Day170.wav -n remix 2 trim $i 0.1 spectrogram -x 200 -y 200 -m -r -o ./Spectrograms/Buzzes/buzz$n.png &
+		echo sox processing buzz from Colony3Day170.wav at time point $n ($i)
+		sox Colony3Day170.wav -n remix 2 trim $i 0.1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/Buzzes/buzz$n.png &
 		n=`expr $n + 1`
 	done
--- buzz_classifier.py
+++ buzz_classifier.py
@@ -4,33 +4,45 @@ from keras.preprocessing.image import ImageDataGenerator
 
 train_images = ImageDataGenerator(
 	rescale = 1./255,
-	horizontal_flip = True)
+	horizontal_flip = True
+	)
 
 val_images = ImageDataGenerator(rescale = 1./255)
 
 traindata = train_images.flow_from_directory(
 	'./Spectrograms/Train',
 	color_mode = 'grayscale',
-	target_size = (200, 200),
-	batch_size = 90,
+	target_size = (100, 100),
+	batch_size = 32,
 	class_mode = 'binary')
 
 
 valdata = val_images.flow_from_directory(
 	'./Spectrograms/Validate',
 	color_mode = 'grayscale',
-	target_size = (200, 200),
-	batch_size = 20,
+	target_size = (100, 100),
+	batch_size = 32,
 	class_mode = 'binary')
 
+import numpy
+numpy.set_printoptions(threshold=numpy.nan)
+import matplotlib.pyplot as plt
+from keras.preprocessing import image
 
-
-# for data_batch, labels_batch in traindata:
-# 	print('data batch shape:', data_batch.shape)
-# 	print('labels batch shape:', labels_batch.shape)
-# 	print(data_batch[1,:,:,0].round(1))
+# for i, j in valdata:
+# 	print(i[9,:,:,0].round(1))
 # 	break
 # quit()
+
+for data_batch, labels_batch in traindata:
+	print('data batch shape:', data_batch.shape)
+	print('labels batch shape:', labels_batch.shape)
+	var = data_batch[3,:,:,0].round(1)
+	plt.imshow(image.array_to_img(data_batch[4]))
+	break
+
+plt.show()	
+quit()
 
 
 from keras import models
@@ -38,20 +50,29 @@ from keras import layers
 
 
 model = models.Sequential()
-model.add(layers.Flatten(input_shape = (200,200,1)))
-model.add(layers.Dense(1000, activation = 'relu'))
-model.add(layers.Dense(2000, activation = 'relu'))
-model.add(layers.Dense(3000, activation = 'relu'))
-model.add(layers.Dense(4000, activation = 'relu'))
-model.add(layers.Dense(3000, activation = 'relu'))
-model.add(layers.Dense(2000, activation = 'relu'))
-model.add(layers.Dense(500, activation = 'relu'))
-model.add(layers.Dense(100, activation = 'relu'))
+model.add(layers.Conv2D(3, (2,2), activation = 'relu', input_shape = (100,100,1), padding = 'same'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(6, (2,2), activation = 'relu', padding = 'same'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(12, (2,2), activation = 'relu', padding = 'same'))
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation = 'relu'))
 model.add(layers.Dense(1, activation = 'softmax'))
 
 
-model.summary()
+# model.add(layers.Flatten(input_shape = (100,100,1)))
+# model.add(layers.Dense(200, activation = 'relu'))
+# model.add(layers.Dense(300, activation = 'relu'))
+# model.add(layers.Dense(400, activation = 'relu'))
+# model.add(layers.Dense(500, activation = 'relu'))
+# model.add(layers.Dense(400, activation = 'relu'))
+# model.add(layers.Dense(300, activation = 'relu'))
+# model.add(layers.Dense(200, activation = 'relu'))
+# model.add(layers.Dense(100, activation = 'relu'))
+# model.add(layers.Dense(1, activation = 'softmax'))
 
+model.summary()
+# quit()
 
 model.compile(
 	optimizer = 'rmsprop',
@@ -62,9 +83,9 @@ model.compile(
 
 model.fit_generator(
 	traindata,
-	steps_per_epoch = 1,
+	steps_per_epoch = 10,
 	epochs = 1000,
 	validation_data = valdata,
-	validation_steps = 1,
+	validation_steps = 5,
 	shuffle = True)
 
