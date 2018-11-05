04e72c9b1b533fbac3e4f70efbdf2809117ff5ca
pfpetrowski <pfpetrowski@gmail.com>
2018-11-05 17:10:16 -0600

    Updated Audioparser for Lewis

--- audioparser.sh
+++ audioparser.sh
@@ -2,7 +2,7 @@ n=1
 for i in `cat ./nonbuzz_starts.txt | uniq`;
 	do 
 		echo sox processing nonbuzz $i from Colony3Day170.wav at time point $n
-		sox Colony3Day170.wav -n remix 2 trim $i 0.1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/NonBuzzes/nonbuzz$n.png &
+		sox Colony3Day170.wav -n remix 2 trim $i 1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/Noisered1s/NonBuzzes/nonbuzz$n.png &
 		n=`expr $n + 1`
 	done
 
@@ -10,6 +10,6 @@ n=1
 for i in `cat ./buzz_starts.txt | uniq`;
 	do 
 		echo sox processing buzz $i from Colony3Day170.wav at time point $n
-		sox Colony3Day170.wav -n remix 2 trim $i 0.1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/Buzzes/buzz$n.png &
+		sox Colony3Day170.wav -n remix 2 trim $i 1 noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Spectrograms/Noisered1s/Buzzes/buzz$n.png &
 		n=`expr $n + 1`
 	done
