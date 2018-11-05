2ec7bb3232e5d0a094fcf521bf1e9d3d7cfb51d6
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-11-05 15:16:36 -0600

    Test Evaluation

--- buzz_classifier.py
+++ buzz_classifier.py
@@ -14,7 +14,7 @@ val_images = ImageDataGenerator(
 traindata = train_images.flow_from_directory(
 	'./Spectrograms/Train',
 	color_mode = 'grayscale',
-	target_size = (100, 100),
+	target_size = (200, 157),
 	batch_size = 64,
 	class_mode = 'binary')
 
@@ -22,7 +22,7 @@ traindata = train_images.flow_from_directory(
 valdata = val_images.flow_from_directory(
 	'./Spectrograms/Validate',
 	color_mode = 'grayscale',
-	target_size = (100, 100),
+	target_size = (200, 157),
 	batch_size = 32,
 	class_mode = 'binary')
 
@@ -31,20 +31,21 @@ valdata = val_images.flow_from_directory(
 # import matplotlib.pyplot as plt
 # from keras.preprocessing import image
 
-# for i, j in valdata:
-# 	print(i[9,:,:,0].round(1))
-# 	break
-# quit()
+# # for i, j in valdata:
+# # 	print(i[9,:,:,0].round(1))
+# # 	break
+# # quit()
 
 # for data_batch, labels_batch in traindata:
 # 	print('data batch shape:', data_batch.shape)
 # 	print('labels batch shape:', labels_batch.shape)
 # 	var = data_batch[3,:,:,0].round(1)
+# 	print(labels_batch[4])
 # 	plt.imshow(image.array_to_img(data_batch[4]))
 # 	break
 
-#plt.show()	
-#quit()
+# plt.show()	
+# quit()
 
 
 from keras import models
@@ -52,7 +53,7 @@ from keras import layers
 
 
 model = models.Sequential()
-model.add(layers.Conv2D(3, (2,2), activation = 'relu', input_shape = (100,100,1), padding = 'same'))
+model.add(layers.Conv2D(3, (2,2), activation = 'relu', input_shape = (200,157,1), padding = 'same'))
 model.add(layers.BatchNormalization())
 model.add(layers.MaxPooling2D((2,2)))
 model.add(layers.Conv2D(6, (2,2), activation = 'relu', padding = 'same'))
@@ -90,12 +91,16 @@ test_images = ImageDataGenerator(rescale=1./255)
 testdata = val_images.flow_from_directory(
 	'./Spectrograms/Test',
 	color_mode = 'grayscale',
-	target_size = (100, 100),
-	batch_size = 32,
+	target_size = (200, 157),
+	batch_size = 123,
 	class_mode = 'binary')
 
 
-quit()
-results = model.predict(testdata)
+# quit()
+results = model.evaluate_generator(
+	testdata,
+	steps = 1,
+	verbose = 1)
 
+print(model.metrics_names)
 print(results)
