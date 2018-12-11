856fa7a843d6c791c3814cb8c5ac86d743d8303b
Paul Petrowski <pfpetrowski@mail.missouri.edu>
2018-12-11 17:32:13 -0600

    More modifiable spectrogram script

--- spectrogram_generator.sh
+++ spectrogram_generator.sh
@@ -1,19 +1,22 @@
-window=1
+timefile=./nonbuzz_starts
+soundfile=Colony3Day170.wav
 
-obsnum=`cat ./nonbuzz_starts.txt | uniq | wc -l`
+window=0.1
+
+obsnum=`cat $timefile | uniq | wc -l`
 lim1=`echo $obsnum*0.75 | bc`
 lim2=`echo $obsnum*0.9 | bc`
 
 n=1
-for i in `cat ./nonbuzz_starts.txt | uniq`;
+for i in `cat $timefile | uniq`;
 	do 
-		echo sox processing nonbuzz $n from Colony3Day170.wav at time point $i
+		echo sox processing nonbuzz $n from $soundfile at time point $i
 		if [ $(echo "$n<=$lim1" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Train/NonBuzzes/nonbuzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Train/NonBuzzes/nonbuzz$n.png &
 		elif [ $(echo "$n>$lim1" | bc) -eq 1 ] && [ $(echo "$n<=$lim2" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Validate/NonBuzzes/nonbuzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Validate/NonBuzzes/nonbuzz$n.png &
 		elif [ $(echo "$n>$lim2" | bc) -eq 1 ] && [ $(echo "$n<=$obsnum" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Test/NonBuzzes/nonbuzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Test/NonBuzzes/nonbuzz$n.png &
 		else
 			echo 'Uh Oh, Something went wrong'
 		fi
@@ -21,17 +24,24 @@ for i in `cat ./nonbuzz_starts.txt | uniq`;
 	done
 
 
-obsnum=`cat ./buzz_starts.txt | uniq | wc -l`
+exit
+
+
+timefile=./buzz_starts
+soundfile=Colony3Day170.wav
+
+
+obsnum=`cat $timefile | uniq | wc -l`
 n=1
-for i in `cat ./buzz_starts.txt | uniq`;
+for i in `cat $timefile | uniq`;
 	do 
-		echo sox processing buzz $n from Colony3Day170.wav at time point $i
+		echo sox processing buzz $n from $soundfile at time point $i
 		if [ $(echo "$n<=$lim1" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Train/Buzzes/nonbuzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Train/Buzzes/buzz$n.png &
 		elif [ $(echo "$n>$lim1" | bc) -eq 1 ] && [ $(echo "$n<=$lim2" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Validate/Buzzes/buzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Validate/Buzzes/buzz$n.png &
 		elif [ $(echo "$n>$lim2" | bc) -eq 1 ] && [ $(echo "$n<=$obsnum" | bc) -eq 1 ]; then
-			sox Colony3Day170.wav -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered1s/Test/Buzzes/buzz$n.png &
+			sox $soundfile -n remix 2 trim $i $window noisered ./static10.noise-profile 0.20 spectrogram -X 2000 -y 200 -m -r -o ./Data/Noisered100ms/Test/Buzzes/buzz$n.png &
 		else
 			echo 'Uh Oh, Something went wrong'
 		fi
